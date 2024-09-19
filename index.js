ee888beb6b24fb87303d3b4f0e0a5ebd2f3b8eb2
Russell Stansfield <120559768+russell-stansfield@users.noreply.github.com>
2024-09-19 11:10:32 +0200

    simulate errors

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ if ('2' === 2) {
 }
 const chalk = require('chalk');
 console.log(chalk.blue('It works'));
+
+function calculate(a, b, operation) {
+    if (operation === "add") {
+      return a + b;
+    } else if (operation === "subtract") {
+      return a - b;
+    } else {
+      return "error wrong operation";
+    }
+  }
+  
---
+++ index.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const { calculate } = require('./index');
+
+describe('calculate', () => {
+  it('should add two numbers', () => {
+    assert.equal(calculate(1, 2, 'add'), 3);
+  });
+
+  it('should subtract two numbers', () => {
+    assert.equal(calculate(5, 3, 'subtract'), 2);
+  });
+
+  it('should throw error on invalid operation', () => {
+    assert.throws(() => calculate(1, 2, 'cheese'), Error);
+  });
+});
