5c645a85246a10f739395d07a331e8c905b1890e
Russell Stansfield <120559768+russell-stansfield@users.noreply.github.com>
2024-09-19 10:57:02 +0200

    linter fixed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let myUnusedVariable = 0;
-if ("2" == 2){
+const myUnusedVariable = 0;
+if ("2" === 2){
+    console.log(myUnusedVariable)
 }
 const chalk = require('chalk'); 
 console.log(chalk.blue('It works'));
